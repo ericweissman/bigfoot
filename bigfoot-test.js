ae31744a0cefbf06236356b67cf3b8485097aa17
Eric Weissman <eric.weissman21@gmail.com>
2019-10-18 11:42:13 -0600

    Makes non-object version w new tests

--- bigfoot-test.js
+++ bigfoot-test.js
@@ -3,12 +3,6 @@ var Bigfoot = require('./bigfoot.js');
 
 
 describe('Bigfoot', function() {
-    it.skip('should be a function and instantiate a Bigfoot', function() {
-      var bigfoot = new Bigfoot();
-      assert.isFunction(Bigfoot);
-      assert.isObject(bigfoot);
-    });
-
     it.skip('should have a name', function() {
       var bigfoot = new Bigfoot('Harry');
       assert.equal(bigfoot.name, 'Harry');
@@ -23,22 +17,6 @@ describe('Bigfoot', function() {
       var bigfoot = new Bigfoot('Sir Nigel', 'damp garbage');
       assert.equal(bigfoot.scent, 'damp garbage');
     });
-
-    it.skip('should start off not having snacked', function() {
-      var bigfoot = new Bigfoot('Lady Grey', 'lavander');
-      assert.equal(bigfoot.appetite, 'starving!');
-      assert.equal(bigfoot.caloriesConsumed, 0);
-      bigfoot.munch();
-      bigfoot.munch();
-      assert.equal(bigfoot.appetite, 'starving!');
-      assert.equal(bigfoot.caloriesConsumed, 2000);
-    });
-
-    function feedingFrenzy(timesEaten, bigfoot) {
-      for (var i = 0; i < timesEaten; i++) {
-        bigfoot.munch();
-      };
-    };
 
     it.skip('should feel some type of way after getting a haircut and have a brand new style', function() {
       var bigfoot = new Bigfoot('Young Ders', 'hotdog juice');
@@ -60,6 +38,50 @@ describe('Bigfoot', function() {
       assert.deepEqual(bigfoot.wardrobe[0], 'tube socks');
     });
 
+    it.skip('should be able to learn basic words', function() {
+      var bigfoot = new Bigfoot('Chuck', 'pepperoni');
+      assert.deepEqual(bigfoot.vocabulary, []);
+      assert.equal(bigfoot.learnWord('apple'), 'I can say - apple!');
+      assert.deepEqual(bigfoot.vocabulary, ['apple']);
+      assert.equal(bigfoot.learnWord('shoes'), 'I can say - shoes!');
+      assert.deepEqual(bigfoot.vocabulary, ['apple', 'shoes']);
+    });
+
+    it.skip('should be able to scream three letter words', function() {
+      var bigfoot = new Bigfoot('Ree', 'burnt toast');
+      assert.equal(bigfoot.learnWord('bug'), 'This is easy... BUG!');
+      assert.deepEqual(bigfoot.vocabulary, ['bug']);
+    });
+
+    it.skip('can only learn "easy" words', function() {
+      var bigfoot = new Bigfoot('Chuck', 'pepperoni');
+      assert.deepEqual(bigfoot.vocabulary, []);
+      assert.equal(bigfoot.learnWord('apple'), 'I can say - apple!');
+      assert.deepEqual(bigfoot.vocabulary, ['apple']);
+      assert.equal(bigfoot.learnWord('train'), 'I can say - train!');
+      assert.deepEqual(bigfoot.vocabulary, ['train','apple']);
+      assert.equal(bigfoot.learnWord('apples'), 'I can only say words that are less than 5 letters!');
+      assert.deepEqual(bigfoot.vocabulary, ['train','apple']);
+      assert.equal(bigfoot.learnWord('trains'), 'I can only say words that are less than 5 letters!');
+      assert.deepEqual(bigfoot.vocabulary, ['train','apple']);
+    })
+
+    it.skip('should start off not having snacked', function() {
+      var bigfoot = new Bigfoot('Lady Grey', 'lavander');
+      assert.equal(bigfoot.appetite, 'starving!');
+      assert.equal(bigfoot.caloriesConsumed, 0);
+      bigfoot.munch();
+      bigfoot.munch();
+      assert.equal(bigfoot.appetite, 'starving!');
+      assert.equal(bigfoot.caloriesConsumed, 2000);
+    });
+
+    function feedingFrenzy(timesEaten, bigfoot) {
+      for (var i = 0; i < timesEaten; i++) {
+        bigfoot.munch();
+      };
+    };
+
     it.skip('should feel content if it eats over 10000 calories', function() {
       var bigfoot = new Bigfoot('Big Country', 'old folks home');
       feedingFrenzy(10, bigfoot);
@@ -77,5 +99,4 @@ describe('Bigfoot', function() {
       feedingFrenzy(20, bigfoot);
       assert.equal(bigfoot.appetite, 'queasy :(');
     });
-
   });
